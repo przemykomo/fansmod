782470cd695c4e3df9082becf00ba85eb3bd6ee6
Przemek <przemek@przemyk.xyz>
2020-05-18 16:58:12 +0200

    Initial commit

---
+++ src/main/java/xyz/przemyk/fansmod/FansMod.java
@@ -0,0 +1,30 @@
+package xyz.przemyk.fansmod;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+
+@Mod(FansMod.MODID)
+public class FansMod {
+    public static final String MODID = "fansmod";
+
+    public FansMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlockRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            LogManager.getLogger().info("HELLO PEKKO");
+        }
+    }
+}
