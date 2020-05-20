7eefda410ef42aebc8d01abcd02d03ff3cd0e824
Przemek <przemek@przemyk.xyz>
2020-05-20 13:45:12 +0200

    update

--- src/main/java/xyz/przemyk/fansmod/blocks/FanTile.java
+++ src/main/java/xyz/przemyk/fansmod/blocks/FanTile.java
@@ -5,7 +5,10 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
+
+import java.util.List;
 
 public class FanTile extends TileEntity implements ITickableTileEntity {
 
@@ -18,81 +21,74 @@ public class FanTile extends TileEntity implements ITickableTileEntity {
 
         this.fanSpeed = fan_speed;
         this.boxLength = boxLength;
-
-        eastBox = new AxisAlignedBB(0, 0, 0, boxLength, 1, 1);
-        westBox = new AxisAlignedBB(0, 0, 0, -boxLength, 1, 1);
-        southBox = new AxisAlignedBB(0, 0, 0, 1, 1, boxLength);
-        northBox = new AxisAlignedBB(0, 0, 0, 1, 1, -boxLength);
-        upBox = new AxisAlignedBB(0, 0, 0, 1, boxLength, 1);
-        downBox = new AxisAlignedBB(0, 0, 0, 1, -boxLength, 1);
     }
 
     private double fanSpeed;
     private double boxLength;
-    
-    private AxisAlignedBB eastBox;
-    private AxisAlignedBB westBox;
-    private AxisAlignedBB southBox;
-    private AxisAlignedBB northBox;
-    private AxisAlignedBB upBox;
-    private AxisAlignedBB downBox;
-
-    @Override
-    public CompoundNBT write(CompoundNBT compound) {
-        compound.putDouble("fanSpeed", fanSpeed);
-        compound.putDouble("boxLength", boxLength);
-        return super.write(compound);
-    }
 
     @Override
     public void read(CompoundNBT compound) {
         super.read(compound);
-        fanSpeed = compound.getDouble("fanSpeed");
-        boxLength = compound.getDouble("boxLength");
-
-        eastBox = new AxisAlignedBB(0, 0, 0, boxLength, 1, 1);
-        westBox = new AxisAlignedBB(0, 0, 0, -boxLength, 1, 1);
-        southBox = new AxisAlignedBB(0, 0, 0, 1, 1, boxLength);
-        northBox = new AxisAlignedBB(0, 0, 0, 1, 1, -boxLength);
-        upBox = new AxisAlignedBB(0, 0, 0, 1, boxLength, 1);
-        downBox = new AxisAlignedBB(0, 0, 0, 1, -boxLength, 1);
+//        switch (getBlockState().getBlock().getRegistryName().getPath()) {
+//            case "iron_fan":
+//                fanSpeed = 0.05;
+//                boxLength = 5;
+//                break;
+//            case "gold_fan":
+//                fanSpeed = 0.1;
+//                boxLength = 5;
+//                break;
+//            case "diamond_fan":
+//                fanSpeed = 0.15;
+//                boxLength = 7;
+//                break;
+//            case "emerald_fan":
+//                fanSpeed = 0.2;
+//                boxLength = 7;
+//                break;
+//            case "redstone_fan":
+//                fanSpeed = 0.13;
+//                boxLength = 6;
+//                break;
+//        }
     }
+
+    private Direction fanDirection;
 
     @Override
     public void tick() {
         if (world != null) {
-            switch (getBlockState().get(BlockStateProperties.FACING)) {
-                case EAST:
-                    for (Entity entity : world.getEntitiesWithinAABB(Entity.class, eastBox.offset(pos))) {
-                        entity.setMotion(entity.getMotion().add(fanSpeed, 0, 0));
-                    }
-                    break;
-                case WEST:
-                    for (Entity entity : world.getEntitiesWithinAABB(Entity.class, westBox.offset(pos))) {
-                        entity.setMotion(entity.getMotion().add(-fanSpeed, 0, 0));
-                    }
-                    break;
-                case SOUTH:
-                    for (Entity entity : world.getEntitiesWithinAABB(Entity.class, southBox.offset(pos))) {
-                        entity.setMotion(entity.getMotion().add(0, 0, fanSpeed));
-                    }
-                    break;
-                case NORTH:
-                    for (Entity entity : world.getEntitiesWithinAABB(Entity.class, northBox.offset(pos))) {
-                        entity.setMotion(entity.getMotion().add(0, 0, -fanSpeed));
-                    }
-                    break;
-                case UP:
-                    for (Entity entity : world.getEntitiesWithinAABB(Entity.class, upBox.offset(pos))) {
-                        entity.setMotion(entity.getMotion().add(0, fanSpeed, 0));
-                    }
-                    break;
-                case DOWN:
-                    for (Entity entity : world.getEntitiesWithinAABB(Entity.class, downBox.offset(pos))) {
-                        entity.setMotion(entity.getMotion().add(0, -fanSpeed, 0));
-                    }
-                    break;
+            fanDirection = getBlockState().get(BlockStateProperties.FACING);
+            AxisAlignedBB scan = new AxisAlignedBB(pos, pos.offset(fanDirection, (int) boxLength).add(1.0, 1.0, 1.0));
+            List<Entity> entityList = world.getEntitiesWithinAABB(Entity.class, scan);
+
+            for (Entity entity : entityList) {
+                addMotion(entity);
+                entity.fallDistance = 0;
             }
+        }
+    }
+
+    private void addMotion(Entity entity) {
+        switch (fanDirection) {
+            case DOWN:
+                entity.setMotion(entity.getMotion().x, entity.getMotion().y - fanSpeed, entity.getMotion().z);
+                break;
+            case UP:
+                entity.setMotion(entity.getMotion().x, entity.getMotion().y + fanSpeed, entity.getMotion().z);
+                break;
+            case NORTH:
+                entity.setMotion(entity.getMotion().x, entity.getMotion().y, entity.getMotion().z - fanSpeed);
+                break;
+            case SOUTH:
+                entity.setMotion(entity.getMotion().x, entity.getMotion().y, entity.getMotion().z + fanSpeed);
+                break;
+            case WEST:
+                entity.setMotion(entity.getMotion().x - fanSpeed, entity.getMotion().y, entity.getMotion().z);
+                break;
+            case EAST:
+                entity.setMotion(entity.getMotion().x + fanSpeed, entity.getMotion().y, entity.getMotion().z);
+                break;
         }
     }
 }
